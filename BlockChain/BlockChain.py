1f3848571b5eac81c3346a7e632ca9f164469d41
Robert Sibanda <robertsibanda20@gmail.com>
2023-11-04 13:07:33 +0200

    created new test files for the Node

--- BlockChain/BlockChain.py
+++ BlockChain/BlockChain.py
@@ -1,13 +1,22 @@
 import random
 import hashlib
-
+import time
+import pymongo
 from Block import Block
+import sys
 
 
+# commented
 class Chain:
 
     def __init__(self, chain):
         self.chain = [self.create_genesis_block()]
+
+    def refresh_block(self):
+        if self.valid_block():
+            print('Block Valid')
+        else:
+            sys.exit(1)
 
     def create_genesis_block(self):
         blok = Block(0, 'Genesis Block')
@@ -18,9 +27,13 @@ class Chain:
         new_block.prev_hash = self.chain[-1].hash
         new_block.hash = self.create_hash(new_block.data)
         self.chain.append(new_block)
+        self.refresh_block()
+
+    def get_last_block(self):
+        return self.chain[-1]
 
     def create_hash(self, data):
-        return hashlib.sha256(data.encode('utf-8')).hexdigest()
+        return hashlib.sha256('{}'.format(data).encode('ascii')).hexdigest()
 
     def valid_block(self):
         for blck in self.chain:
@@ -33,4 +46,3 @@ class Chain:
             if blck.hash != self.create_hash(blck.data):
                 return False
         return True
-
